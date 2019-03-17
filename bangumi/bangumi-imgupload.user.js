f9e2870b5a6583d0c4d59436c79cc94dd395ea94
JING JIAN <uzumaki@yeah.net>
2019-03-17 20:15:30 +0800

    Update bangumi-imgupload.user.js

--- bangumi/bangumi-imgupload.user.js
+++ bangumi/bangumi-imgupload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bangumi图片上传
 // @namespace    https://github.com/shadowdreamer/jioben
-// @version      0.5
+// @version      0.6
 // @description  上传图片到niupic，catbox,管理历史上传记录
 // @author       dovahkiin
 // @include      http*://bgm.tv*
@@ -26,7 +26,7 @@
                 open: false,
                 openHistory: false,
                 urlList: [],
-                uploadApi: 'niupic',
+                uploadApi: 'catbox',
             };
         },
         computed: {
