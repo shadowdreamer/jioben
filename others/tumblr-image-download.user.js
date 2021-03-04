a9e65d578dfecaafa91e46132e4b94ea0cc167c3
shadowdreamer <uzumaki@yeah.net>
2021-03-04 18:00:03 +0800

    add state hint

--- others/tumblr-image-download.user.js
+++ others/tumblr-image-download.user.js
@@ -12,25 +12,32 @@
 // ==/UserScript==
 
 (function(){
-    let btn = document.createElement('li')
+    const btn = document.createElement('li')
     btn.innerHTML = '<a>下载图片</a>'
     btn.href = "javascript:void(0)"
     btn.setAttribute('style','cursor:pointer')
-    document.querySelector('.post_info').append(btn)
     btn.addEventListener('click',startDownload)
     
+    const msg = document.createElement('li')
+
+    document.querySelector('.post_info').append(btn)
+    document.querySelector('.post_info').append(msg)
+
     async function startDownload(){
-        const files = await getFileNameAndBlob();
+        let i = 0
+        msg.innerText = '开始保存'
+
         const dirHandler = await window.showDirectoryPicker()
+        const files = await getFileNameAndBlob();
         for(const file of files){
             const fileHandler = await dirHandler.getFileHandle(file.fileName,{create:true})
             const writable = await fileHandler.createWritable()
             await writable.write(file.blob);
             await writable.close()
+            msg.innerText = `保存中-${++i}/${files.length}`
         }
-
+        msg.innerText = `保存完毕`
     }
-
     async function getFileNameAndBlob(){
         const nodeList = document.querySelector('iframe.photoset').contentDocument
         .querySelectorAll('img');
@@ -49,7 +56,9 @@
                 blob,
                 fileName:`${fileName}-${list.length+1}.${suffix}`
             })
+            msg.innerText = `下载中-${list.length+1}/${nodeList.length}`
         }
         return list;
     }
+
 })()
