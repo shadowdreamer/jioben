46fe97f050e6e18234820f94526b0493a9512f21
shadowdreamer <git@dovahkiin.top>
2024-06-27 15:10:51 +0800

    bgm virtual list

--- bangumi/abort-ep-image-load.user.js
+++ bangumi/abort-ep-image-load.user.js
@@ -5,38 +5,71 @@
 // @description  顺便加图片查看view.js
 // @author       cureDovahkiin
 // @include      /^https?://(bgm\.tv|bangumi\.tv|chii\.in)\/ep\/.*
-// @require      https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.6/viewer.min.js
-// @resource     IMPORTED_CSS https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.6/viewer.min.css
-// @grant        GM_getResourceText
-// @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
   'use strict';
-  const viewercss = GM_getResourceText("IMPORTED_CSS");
-  GM_addStyle(viewercss);
   let images = document.querySelectorAll(".reply_content img.code");
-  images.forEach(el => {
-    let s = el.src;
-    el.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEAAAAALAAAAAABAAEAAAI=;'
-    let btn = $(`<a href='javascript:;' style="color:blue"></a>`);
-    btn.text(`[加载图片]`);
-    btn.attr("title", s);
-    const vw = new Viewer(el);
-    $(el).after(btn).attr('loading','lazy').detach()
-      .on('click', function () {
-        vw.show();
+  let ep_image_count = window.localStorage.getItem("ep_image_count") || 0;
+  let box = $(`<input type="number" class="ep_image_count"/>`).on('change', function () {
+    ep_image_count = $(this).val();
+    window.localStorage.setItem("ep_image_count", ep_image_count);
+  }).val(ep_image_count)
+  let warp = $(`<div>默认加载图片数，-1为全部显示：</div>`);
+  warp.css('background', '#f2f2f2')
+  warp.css('display', 'inline-flex')
+  warp.css('padding', '4px 0')
+  warp.css('opacity', '0.7')
+  warp.append(box);
+  $('.singleCommentList').before(warp)
+  images.forEach((el,i) => {
+    let vw = null;
+    function initBtn(){
+      let s = el.src;
+      el.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEAAAAALAAAAAABAAEAAAI=;'
+      let btn = $(`<a href='javascript:;' style="color:#0084b4"></a>`);
+      btn.text(`[加载图片]`);
+      btn.attr("title", s);
+      btn.on("click", function () {
+        btn.text(`[加载中...url:${s}]`)
+        el.src = s;
+        btn.after(el);
+        $(el).on('click', function () {
+          initViewer();
+        }).on('error', function () {
+          btn.text(`[加载失败！url:${s}]`)
+        }).on('load', function () {
+          btn.remove();
+        })
       })
-    btn.on("click", function () {
-      el.onload = function () {
-        btn.remove();
+      $(el).after(btn).detach()
+    }
+    $(el).attr('loading', 'lazy');
+    function initViewer(){
+      if(!vw){
+        vw = new Viewer(el,{
+          navbar:false,
+        });
       }
-      el.onerror = function () {
-        btn.text(`[加载失败:url:${s}]`)
-      }
-      btn.text("[加载中。。]")
-      el.src = s;
-      btn.after(el)
-    })
+      vw.show();
+    }
+    if(ep_image_count>=0 && i+1>ep_image_count){
+      initBtn();
+    }else{
+      $(el).on('click', function () {
+        initViewer();
+      })
+    }
   })
+  // load js
+  var script = document.createElement("script");
+  script.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.6/viewer.min.js");
+  document.head.appendChild(script);
+  // load css url
+  let viewercss = "https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.6/viewer.min.css";
+  var link = document.createElement("link");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("type", "text/css");
+  link.setAttribute("href", viewercss);
+  document.head.appendChild(link);
 })();
---
+++ bangumi/bgm-virtual-scroll.user.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @name         虚拟列表
+// @namespace    https://github.com/shadowdreamer/jioben/tree/master/bangumi
+// @version      0.1
+// @description  -
+// @author       cureDovahkiin
+// @include      /^https?://(bgm\.tv|bangumi\.tv|chii\.in)\/ep\/.*
+// ==/UserScript==
+
+
+(function () {
+  'use strict';
+  const warp = $(`#comment_list`);
+  warp.css("min-height", `${warp.height()}px`)
+    .css("position", `relative`);
+  $('.row_state.clearit')
+    .css("position", `absolute`)
+    .css("bottom", `0px`)
+    .css('width', '100%')
+  const list = $(`#comment_list>.row_reply`)
+  let wh = window.innerHeight;
+  window.addEventListener("resize", function () {
+    wh = window.innerHeight;
+  })
+  const heightStore = {
+    map: [],
+    getByHeight: function (h) {
+      let i = 0, len = this.map.length;
+      for (; i < len; i++) {
+        if (h <= this.map[i].top) break;
+      };
+      return i > 0 ? i - 1 : i;
+    },
+    getActiveIndex: function (center) {
+      let around = [center], len = this.map.length,
+        prevH = 0, nextH = 0;
+      for (let i = 1; i < center + 1; i++) {
+        prevH += this.map[center - i].height;
+        around.unshift(center - i);
+        if (prevH >= wh * 2) break;
+      }
+      for (let i = 1; i < len - center; i++) {
+        nextH += this.map[center + i].height;
+        around.push(center + i);
+        if (nextH >= wh * 2) break;
+      }
+      return around
+    },
+
+  }
+
+
+  let _dh = 0;
+  list.each((i, el) => {
+    let $el = $(el);
+    heightStore.map[i] = {
+      height: $el.height(),
+      top: _dh
+    };
+    _dh += heightStore.map[i].height + 20;
+    $el.css('position', 'absolute')
+      .css('width', '100%')
+      .css('box-sizing', 'border-box');
+  })
+  list.detach();
+  let last_around = []
+  const ob = new MutationObserver(() => {
+    let offset = 0;
+    last_around.forEach((i) => {
+      let { height, top } = heightStore.map[i];
+      list.eq(i).css('transform', `translateY(${heightStore.map[i].top + offset}px)`);
+      offset+=list.eq(i).height()-height;
+    })
+
+  });
+  ob.observe(warp[0], { childList: true, subtree: true });
+  $(document).on("scroll", function () {
+    let st = $(document).scrollTop();
+    let index = heightStore.getByHeight(st);
+    let around = heightStore.getActiveIndex(index);
+    let newIndex = around.filter((i) => !last_around.includes(i));
+    let delIndex = last_around.filter((i) => !around.includes(i));
+    last_around = around;
+    console.log(index, around);
+    delIndex.forEach((i) => {
+      list.eq(i).detach();
+    })
+    newIndex.forEach((i) => {
+      let node = list.eq(i)
+      node.css("transform", `translateY(${heightStore.map[i].top}px)`).appendTo(warp);
+    })
+  })
+})()
